9afa5f7fea1eb6cc3133c6a1140b07b573acbf4f
LeoLizc <ldlizcano@uninorte.edu.co>
2023-07-09 21:07:22 -0500

    Initial commit

---
+++ src/com/leolizc/particleSystem/Main.java
@@ -0,0 +1,23 @@
+package com.leolizc.particleSystem;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+    public void settings(){
+        size(600,600);
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+    }
+
+    public static void main(String[] args){
+        PApplet.main("com.leolizc.particleSystem.Main");
+    }
+}
