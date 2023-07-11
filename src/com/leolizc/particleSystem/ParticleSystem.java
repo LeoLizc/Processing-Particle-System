38771138023f263498ac2c5515f89cee382a16d5
LeoLizc <ldlizcano@uninorte.edu.co>
2023-07-11 01:02:35 -0500

    Update ParticleSystem

--- src/com/leolizc/particleSystem/ParticleSystem.java
+++ src/com/leolizc/particleSystem/ParticleSystem.java
@@ -1,5 +1,36 @@
 package com.leolizc.particleSystem;
 
-public class ParticleSystem {
+import processing.core.PVector;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public abstract class ParticleSystem {
+    PVector position;
+    LinkedList<Particle> particles;
+
+    public ParticleSystem(PVector position){
+        this.position = position.copy();
+        this.particles = new LinkedList<Particle>();
+    }
+
+    public abstract void addParticle();
+
+    public void run(){
+        ListIterator<Particle> iterator = particles.listIterator();
+        while(iterator.hasNext()){
+            Particle particle = iterator.next();
+            particle.render();
+            particle.updatePhysics();
+            if(particle.isDead()){
+                iterator.remove();
+            }
+        }
+    }
+
+    public void applyForce(PVector force){
+        for(int i = 0; i < particles.size(); i++){
+            particles.get(i).applyForce(force);
+        }
+    }
 }
