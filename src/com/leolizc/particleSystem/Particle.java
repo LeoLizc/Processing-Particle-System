5275772421fe292558967ee50616327b7ac87423
LeoLizc <ldlizcano@uninorte.edu.co>
2023-07-12 01:48:07 -0500

    fixing the force application

--- src/com/leolizc/particleSystem/Particle.java
+++ src/com/leolizc/particleSystem/Particle.java
@@ -3,31 +3,52 @@ package com.leolizc.particleSystem;
 import processing.core.PApplet;
 import processing.core.PVector;
 
+import java.util.Arrays;
+
 public abstract class Particle {
     float lifeSpan;
-    PVector position, velocity, acceleration;
+    PVector position;
+    PVector velocity;
+    PVector acceleration;
+    PVector[] forces;//acceleration is an always applied force
 
     public Particle(PVector position) {
         this.position = position.copy();
         this.velocity = new PVector(0, 0);
         this.acceleration = new PVector(0, 0.05f);
         this.lifeSpan = 255;
+        this.forces = new PVector[10];
     }
 
     public abstract void render(PApplet p);
 
+    public void applyForce(PVector force) {
+        for (int i = 0; i < forces.length; i++) {
+            if (forces[i] == null) {
+                forces[i] = force.copy();
+                return;
+            }
+        }
+    }
+
     public void updatePhysics() {
+        PVector acceleration = this.acceleration.copy();
+        for (PVector force : forces) {
+            if (force != null) {
+                acceleration.add(force);
+            }
+        }
         velocity.add(acceleration);
         position.add(velocity);
         lifeSpan -= 2.0;
+        // Remember that this.acceleration is an always applied force
+        // so, we don't need to reset it
+        Arrays.fill(forces, null);
+
     }
 
     public boolean isDead() {
         return lifeSpan < 0.0;
-    }
-
-    public void applyForce(PVector force) {
-        acceleration.add(force);
     }
 
     public abstract Particle copy();
